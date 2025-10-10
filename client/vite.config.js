00f248f38c92960f7a39d4e2870c7b34be1dbd22
Kiana Pillay <ST10258543@vcconnect.edu.za>
2025-10-10 20:35:51 +0200

    Added SSL

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    https: {
+      key: fs.readFileSync(path.resolve('../server/ssl/privatekey.pem')),
+      cert: fs.readFileSync(path.resolve('../server/ssl/certificate.pem'))
+    },
+    port: 5173
+  }
 })
--- server/server.js
+++ server/server.js
@@ -12,14 +12,17 @@ app.use(express.json());
 app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-//const options = {
-  //key: fs.readFileSync("./ssl/privatekey.pem"),
-  //cert: fs.readFileSync("./ssl/certificate.pem")
-//};
 
-//https.createServer(options, app).listen(PORT, () => {
-  //console.log(`Secure backend running at https://localhost:${PORT}`);
-//});
-app.listen(PORT, () => {
-  console.log(`Secure Backend running at http://localhost:${PORT}`);
+//SSL configuration 
+const options = {
+  key: fs.readFileSync("./ssl/privatekey.pem"),
+  cert: fs.readFileSync("./ssl/certificate.pem")
+};
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Secure backend running at https://localhost:${PORT}`);
 });
+
+//app.listen(PORT, () => {
+  //console.log(`Secure Backend running at http://localhost:${PORT}`);
+//});
