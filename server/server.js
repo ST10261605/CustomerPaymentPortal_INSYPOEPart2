9a57d6dfbae006742ca87fc4bc0109f9fa2f3e6f
Kiana Pillay <ST10258543@vcconnect.edu.za>
2025-11-07 20:51:33 +0200

    Updated and implemented full security

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import https from "https";
+import http from "http";
 import fs from "fs";
 import dotenv from "dotenv";
 import app from "./app.js";
@@ -15,6 +16,21 @@ const options = {
   key: fs.readFileSync("./ssl/privatekey.pem"),
   cert: fs.readFileSync("./ssl/certificate.pem")
 };
+
+// HTTP to HTTPS redirect app
+const httpApp = express();
+httpApp.use((req, res) => {
+  console.log(`Redirecting HTTP to HTTPS`);
+  res.redirect(301, `https://localhost:${PORT}${req.url}`);
+});
+
+// Start HTTP server on port 8080 (should work based on netstat)
+const httpServer = http.createServer(httpApp);
+
+httpServer.listen(80, 'localhost', () => {
+  console.log('HTTP redirect server running on http://localhost:80');
+});
+
 
 https.createServer(options, app).listen(PORT, () => {
   console.log(`Secure backend running at https://localhost:${PORT}`);
